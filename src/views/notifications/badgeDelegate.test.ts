87dc5f2f63e4aead2404f044a91e9d361568f830
Christian Abella <cabella@atlassian.com>
2025-05-30 14:31:42 -1000

    AXON-379: badge color + banner action analytics

--- src/views/notifications/badgeDelegate.test.ts
+++ src/views/notifications/badgeDelegate.test.ts
@@ -186,9 +186,9 @@ describe('BadgeDelegate', () => {
             NotificationSurface.Badge,
         );
         expect(decorationUri1).toEqual({
-            badge: '2️⃣',
+            badge: '2',
             tooltip: '2 notifications',
-            color: new ThemeColor('editorForeground'),
+            color: new ThemeColor('activityBarBadge.background'),
             propagate: false,
         });
 
@@ -197,9 +197,9 @@ describe('BadgeDelegate', () => {
             NotificationSurface.Badge,
         );
         expect(decorationUri2).toEqual({
-            badge: '1️⃣',
+            badge: '1',
             tooltip: '1 notification',
-            color: new ThemeColor('editorForeground'),
+            color: new ThemeColor('activityBarBadge.background'),
             propagate: false,
         });
     });
